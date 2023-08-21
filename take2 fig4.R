5bc1214171cbd352561a4e50985e70cb57e1ce4b
bearsofthemoss <43791909+bearsofthemoss@users.noreply.github.com>
2023-08-21 10:08:01 -0400

    figure update without 3 models

--- take2 fig4.R
+++ take2 fig4.R
@@ -1,9 +1,14 @@
+
+library(readxl)
+library(reshape2)
+library(ggplot2)
+
 #for figure 4)   (same datafile though-  Alex_re_arrange_4_29)
-c<-  as.data.frame(read_excel("Alex_re_arrange_4_29.xlsx"))
+c<-  as.data.frame(read_excel("Data_folder/Alex_re_arrange_4_29.xlsx"))
 c<-c[!c$model=="M2",]
 table(c$distribution)
 
-
+table(c$distribution, c$model)
 
 c[c$distribution=="triangle shaped","distribution"]<-"not used"
 c[c$distribution=="Left truncated triangles","distribution"]<-"not used"
@@ -23,6 +28,7 @@ c[c$distribution=="Left truncated uniform","distribution"]<- "Truncated uniform 
 
 
 # melt data # from dataframe 'c'
+
 cm<-melt(c, id.vars = c("model","structure","distribution","rows"))  
 head(cm) 
 table(cm$distribution)
@@ -49,15 +55,48 @@ median(cm$value)
 
 summary(cm$log_value)
 
-my_breaks = c(0.01, .1,  .5,  .95)
-
+my_breaks = c(1, 10,  50,  95)
 color_scale <- rev(colorRampPalette(c('darkred','red', 'yellow', 'orange', '#78C679', '#41AB5D', '#238443', 'forestgreen',"darkgreen"))(8))
+
+
+head(cm)
+
+## 8/21
+## drop model 1, 3, 4.  Then show columns as distributions
+
+# 
+di <- cm[cm$model=="Model 1", ]
+
+library(scales)
+
+
+di[di$structure=="arbogast","structure"] <- "Reverse-J"
+di[di$structure=="uncless","structure"] <- "Even-aged distribution"
+di[di$structure=="uni","structure"] <- "Uniform"
+
+
+ggplot(di , aes(x=variable, y=rows))+
+  geom_tile(aes(fill=value*100), colour=NA)+
+  scale_fill_gradientn(name="Absolute Uncertainty (%)",colors = color_scale, trans = "log10",breaks = my_breaks, labels = my_breaks) +
+  ylab("Sampling strategy")+ xlab("Sample size") +
+  theme(panel.grid.minor = element_line( colour ="black", linetype ="dotted", size = 0.5)) +
+  theme(panel.background = element_rect( colour ="black", fill ="white" ,size = 0.5 )) +
+  theme(plot.title = element_text(size = 25, face = "bold"))+
+  facet_grid(distribution ~ structure, scales="free_y", 
+             labeller = label_wrap_gen(width=10),
+             space="free")+
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust=.5))+
+  theme(plot.title = element_text(hjust = 0.5),strip.text.y = element_text(size = 8, angle = 0))+
+  scale_y_continuous(breaks= seq(1,8,1))+
+  theme(text = element_text(family = "Calibri"))
+
+
+###########################################################################
 
 
 fa<-ggplot(cm[cm$structure=="uncless",], aes(x=variable, y=rows))+
   geom_tile(aes(fill=value), colour=NA)+
-  scale_fill_gradientn(name="",colors = color_scale, trans = "log10",
-                       breaks = my_breaks, labels = my_breaks) +
+  scale_fill_gradientn(name="",colors = color_scale, trans = "log10",breaks = my_breaks, labels = my_breaks) +
   ylab("Sampling strategy")+
   theme(panel.grid.minor = element_line( colour ="black", linetype ="dotted", size = 0.5)) +
   theme(panel.background = element_rect( colour ="black", fill ="white" ,size = 0.5 )) +theme(plot.title = element_text(size = 25, face = "bold"))+
